f729a76cdbfa67f832c042a76768079c25bfd797
Aakarshit Sharma <mngmnt.mains@gmail.com>
2025-06-07 12:28:50 +0530

    feat: added 7 Wonders of the World markers with popups

---
+++ js/data.js
@@ -0,0 +1,30 @@
+export const wonders = [
+  {
+    name: "Great Wall of China",
+    coords: [40.4319, 116.5704],
+  },
+  {
+    name: "Petra",
+    coords: [30.3285, 35.4444],
+  },
+  {
+    name: "Christ the Redeemer",
+    coords: [-22.9519, -43.2105],
+  },
+  {
+    name: "Machu Picchu",
+    coords: [-13.1631, -72.5450],
+  },
+  {
+    name: "Chichen Itza",
+    coords: [20.6843, -88.5678],
+  },
+  {
+    name: "Roman Colosseum",
+    coords: [41.8902, 12.4922],
+  },
+  {
+    name: "Taj Mahal",
+    coords: [27.1751, 78.0421],
+  },
+];
--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-const map = L.map('map').setView([20.5937, 78.9629], 5); // India coordinates
+import { wonders } from "./data.js";
+
+// Initialize the map and set its view to a specific geographical location and zoom level
+const map = L.map('map').setView([20.0, 0.0], 2);
 
 // Load tiles (map graphics) from OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+wonders.forEach(wonder => {
+  L.marker(wonder.coords)
+    .addTo(map)
+    .bindPopup(`<h2 class="text-lg font-bold">${wonder.name}</h2>`);
+});
